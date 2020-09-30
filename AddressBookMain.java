2af8333052c5b752e269b9ab49981212d70ff60f
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-30 21:27:17 +0530

    UC 3

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,30 +1,95 @@
 import java.util.*;
 public class AddressBookMain{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Welcome to Address Book Program");
-        Contact con=new Contact();
-        List<Contact> addressBook = new ArrayList<Contact>();
-        char ex='Y';
-        while(ex=='Y'){
+    List<Contact> addressBook = new ArrayList<Contact>();
+    static Scanner sc=new Scanner(System.in);
+    public void addContact() {
+
+        Contact con = new Contact();
+        char ex = 'Y';
+        while (ex == 'Y') {
             System.out.println("Enter Contact Details\nEnter First Name:");
-                con.setFirstName(sc.next());
+            con.setFirstName(sc.next());
             System.out.println("Enter Last Name:");
-                con.setLastName(sc.next());
+            con.setLastName(sc.next());
             System.out.println("Enter Address:");
-                con.setAddress(sc.next());
+            con.setAddress(sc.next());
             System.out.println("Enter Zip Code:");
-                con.setZip(sc.next());
+            con.setZip(sc.next());
             System.out.println("Enter Phone Number:");
-                con.setPhoneNo(sc.next());
+            con.setPhoneNo(sc.next());
             System.out.println("Enter Email Address:");
-                con.setEmail(sc.next());
+            con.setEmail(sc.next());
             System.out.println("Add new Contact: (Y/N ):");
-            ex=sc.next().charAt(0);
+            ex = sc.next().charAt(0);
+            addressBook.add(con);
+        }
+    }
+    public void editContact(String name) {
+        boolean exitFlag=true;
+        for(Contact c:addressBook) {
+            if(c.getFirstName().equals(name)) {
+                while (exitFlag) {
+                    System.out.println("To edit enter Number:\n 1.firstName\n 2.lastName\n 3.address\n 4.zip\n 5.phoneNo\n 6.email\n 7.exit");
+                    int check = sc.nextInt();
+                    switch (check) {
+                        case 1:
+                        {System.out.println("Enter First Name:");
+                            String firstname = sc.next();
+                            c.setFirstName(firstname);
+                            break;}
 
+                        case 2:
+                        {System.out.println("Enter Last Name:");
+                            String lastname = sc.next();
+                            c.setLastName(lastname);
+                            break;}
 
+                        case 3:
+                        {System.out.println("Enter the Address:");
+                            String addr = sc.next();
+                            c.setAddress(addr);
+                            break;}
+
+                        case 4:
+                        {System.out.println("Enter Zip:");
+                            String zipcode = sc.next();
+                            c.setZip(zipcode);
+                            break;}
+
+                        case 5:
+                        {System.out.println("Enter Phone Number:");
+                            String phone = sc.next();
+                            c.setPhoneNo(phone);
+                            break;}
+
+                        case 6:
+                        {System.out.println("Enter Email Id:");
+                            String mail = sc.next();
+                            c.setEmail(mail);
+                            break;}
+
+                        case 7:
+                        {System.out.println("Exiting edit section");
+                            exitFlag = false;
+                            break;}
+
+                        default:
+                            System.out.println("Enter number from 1 to 7");
+                    }
+                }
+            }else
+                System.out.println("Contact with "+name+" doesnot exist.");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Welcome to Address Book Program");
+        AddressBookMain abm = new AddressBookMain();
+        abm.addContact();
+        System.out.println("Enter name of contact to be edited:");
+        String nam=sc.next();
+        abm.editContact(nam);
 
         }
 
-    }
+
 }
